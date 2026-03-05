9aff3e542228a5c7e50db1b2ade83a8047889c8b
Shubham <shubhamp0230@gmail.com>
2026-03-05 05:30:01 +0000

    1st commit

---
+++ src/main/java/com/demo_devops/Democlass.java
@@ -0,0 +1,11 @@
+package com.demo_devops;
+
+
+public class Democlass
+{
+
+        public static void main(String[] args) {
+            System.out.println("Hello DevOps, this is 1st demo!");
+        }
+
+}
